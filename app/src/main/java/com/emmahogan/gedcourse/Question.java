5467869ea34922ee5025dd9fd01dee370a534953
Emma Hogan <em30hoga@siena.edu>
2020-12-21 20:20:53 -0500

    Create constructor for Question class

--- app/src/main/java/com/emmahogan/gedcourse/Question.java
+++ app/src/main/java/com/emmahogan/gedcourse/Question.java
@@ -4,5 +4,12 @@ public class Question {
 
     public String question;
     public String answer;
+    public boolean multiple_choice;
+
+    public Question(String question, String answer, boolean multiple_choice) {
+        this.question = question;
+        this.answer = answer;
+        this.multiple_choice = multiple_choice;
+    }
 
 }
