c71eecd09818bea178e90438cea51318ee620dfa
Emma Hogan <em30hoga@siena.edu>
2020-12-31 11:21:10 -0500

    Create animation video for Place Value and custom video layout

--- app/src/main/java/com/emmahogan/gedcourse/LessonView.kt
+++ app/src/main/java/com/emmahogan/gedcourse/LessonView.kt
@@ -1,10 +1,13 @@
 package com.emmahogan.gedcourse
 
+import android.app.Dialog
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.VideoView
 
 class LessonView : AppCompatActivity() {
 
@@ -21,6 +24,7 @@ class LessonView : AppCompatActivity() {
         lesson = Lesson(applicationContext,unit_num,lesson_num)
 
         val lessonview_title = findViewById<TextView>(R.id.lessonview_title)
+        val watch_anim_btn = findViewById<Button>(R.id.watch_anim_btn)
         val lessonview_content = findViewById<TextView>(R.id.lessonview_content)
         val lessonview_example = findViewById<TextView>(R.id.lessonview_example)
         val lessonview_citation = findViewById<TextView>(R.id.lessonview_citation)
@@ -30,6 +34,16 @@ class LessonView : AppCompatActivity() {
         lessonview_content.text = lesson.content
         lessonview_example.text = lesson.example
         lessonview_citation.text = lesson.citation
+
+        watch_anim_btn.setOnClickListener {
+            val alert = Dialog(this)
+            alert.setContentView(R.layout.lesson_animation)
+
+            val videoPlayer = alert.findViewById<VideoView>(R.id.videoPlayer)
+            val vid_uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.anim1_2)
+            videoPlayer.setVideoURI(vid_uri)
+            alert.show()
+        }
 
         next_btn.setOnClickListener {
 
