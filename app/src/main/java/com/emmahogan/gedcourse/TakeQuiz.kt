caa98be7631fb799ec96c44bdc785640e619073f
Emma Hogan <em30hoga@siena.edu>
2021-01-10 21:38:31 -0800

    Create layouts for starting quiz and taking quiz

---
+++ app/src/main/java/com/emmahogan/gedcourse/TakeQuiz.kt
@@ -0,0 +1,11 @@
+package com.emmahogan.gedcourse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TakeQuiz : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_take_quiz)
+    }
+}
