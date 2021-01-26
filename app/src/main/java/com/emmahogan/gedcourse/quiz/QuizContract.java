dedf7ac6fe486597ffbb19a351aa594d06984791
Emma Hogan <em30hoga@siena.edu>
2021-01-26 13:12:41 -0500

    Add practice questions for sections 1.1 and 1.2

--- app/src/main/java/com/emmahogan/gedcourse/quiz/QuizContract.java
+++ app/src/main/java/com/emmahogan/gedcourse/quiz/QuizContract.java
@@ -4,10 +4,20 @@ import android.provider.BaseColumns;
 
 public final class QuizContract {
 
-    private QuizContract() {}
+    private int unit_num;
+    private int lesson_num;
+
+    private static String qc_table_name = "quiz_questions";
+
+    private QuizContract(int unit_num, int lesson_num) {
+        this.unit_num = unit_num;
+        this.lesson_num = lesson_num;
+
+        qc_table_name = "questions_" + unit_num + "_" + lesson_num;
+    }
 
     public static class QuestionsTable implements BaseColumns {
-        public static final String TABLE_NAME = "quiz_questions";
+        public static final String TABLE_NAME = qc_table_name;
         public static final String COLUMN_QUESTION = "question";
         public static final String COLUMN_OPTION1 = "option1";
         public static final String COLUMN_OPTION2 = "option2";
